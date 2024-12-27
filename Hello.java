f3d0d1e398d75096cf612886e3f4b77e78836035
chandana-523 <22pa1a0523@vishnu.edu.in>
2024-12-27 10:26:14 +0530

    Add Hello.java file

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello{
+    public static void main(String[] args) {
+        System.out.println("Hello this is my first git attachment");
+    }
+
+    @Override
+    public String toString() {
+        return "Hello []";
+    }
+
+}
